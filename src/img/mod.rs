755838b4cbc99ea92ada1b8c53923c6ee3a4ec14
guangzefrio <guangzefrio@163.com>
2022-05-05 14:21:30 +0800

    image process

--- src/img/mod.rs
+++ src/img/mod.rs
@@ -6,12 +6,23 @@ use std::{fs::OpenOptions, path::Path, sync::Once};
 use magick_rust::{magick_wand_genesis, MagickWand};
 use strum_macros::{AsRefStr, EnumString};
 
+// 图片中关于图像的属性
+const DATETIME_PROP: [&'static str; 5] = [
+    "exif:DateTime",         // 2016:03:17 12:43:55
+    "exif:DateTimeOriginal", // 2016:03:17 12:43:55
+    "exif:GPSDateStamp",     // 2016:03:17
+    "date:create",           // 2022-05-05T03:48:40+00:00
+    "date:modify",           // 2022-05-05T03:48:40+00:00
+];
+const SIGNATURE_PROP: [&'static str; 1] = ["signature"];
+
 // 只执行一次，初始化image magic
 static START: Once = Once::new();
 
 #[derive(Debug)]
 pub struct ImageMeta {
     create_time: Option<DateTime<Local>>,
+    gps: (f64, f64),
 }
 
 #[derive(Debug, PartialEq, EnumString, AsRefStr)]
@@ -24,14 +35,41 @@ pub enum ImageFormat {
     Png,
 }
 
+pub fn retrive_img_meta(path: &Path) -> Result<ImageFormat, AnyError> {
+    START.call_once(|| {
+        magick_wand_genesis();
+    });
+    let wand = MagickWand::new();
+    wand.read_image(
+        path.to_str()
+            .ok_or(AnyError::msg("Convert path to str failed!"))?,
+    )?;
+    return Err(AnyError::msg("error"));
+}
+
+// 信息获取的有效性
+enum InfoValidScore {
+    High,
+    Middle,
+    Low,
+}
+
+// 1. 尝试从图片中获取时间相关信息
+// 2. 从文件名中获取时间相关信息
+// 3. 根据图片的创建时间获取
+fn try_retrieve_img_create_time_info(
+    img: &MagickWand,
+) -> Result<(DateTime<Local>, InfoValidScore), AnyError> {
+    return Err(AnyError::msg("无法判断图片具体的创建时间"));
+}
+
 pub fn change_img_format(path: &Path, format: ImageFormat) -> Result<Vec<u8>, AnyError> {
     START.call_once(|| {
         magick_wand_genesis();
     });
     let wand = MagickWand::new();
     let res = wand.read_image(
-        path.as_os_str()
-            .to_str()
+        path.to_str()
             .ok_or(AnyError::msg("Path osstr to str failed"))?,
     );
     match res {
