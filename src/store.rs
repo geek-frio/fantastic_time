24ba630e0d423028f35a7a2987dcbf742f7652b0
geek-frio <guangzefrio@gmail.com>
2022-08-01 14:43:58 +0800

    Dao implementation complete

--- src/store.rs
+++ src/store.rs
@@ -6,9 +6,25 @@ use crate::conf::GlobalConfig;
 
 #[derive(Debug)]
 pub struct ImgMetaDO {
-    pub(crate) id: Option<String>,
-    pub(crate) time: NaiveDateTime,
-    pub(crate) sign: String,
+    id: Option<String>,
+    time: NaiveDateTime,
+    sign: String,
+}
+
+impl ImgMetaDO {
+    pub fn new(time: NaiveDateTime, sign: String) -> ImgMetaDO {
+        ImgMetaDO {
+            id: None,
+            time,
+            sign,
+        }
+    }
+
+    pub fn with_id(id: String, time: NaiveDateTime, sign: String) -> ImgMetaDO {
+        let mut img = Self::new(time, sign);
+        img.id = Some(id);
+        img
+    }
 }
 
 pub struct ImgMetaDao {
@@ -18,41 +34,38 @@ pub struct ImgMetaDao {
 impl ImgMetaDao {
     pub fn create_table(conn: &Connection) -> DbResult<usize> {
         let res = conn.execute(
-            "CREATE TABLE img_meta (id integer primary key, time, timestamp, sign)",
+            "CREATE TABLE img_meta (id primary key, time, timestamp, sign)",
             [],
         );
         res
     }
 
-    pub fn batch_write(&self, domains: Vec<ImgMetaDO>) -> Result<(), Vec<ImgMetaDO>> {
-        let conn = self.get_conn();
-
-        let mut failed_records = Vec::new();
-
+    pub fn batch_write(domains: Vec<ImgMetaDO>, conn: &Connection) -> Result<(), anyhow::Error> {
         for domain in domains {
+            tracing::info!("domain.time:{:?}", domain.time);
+
             let (time, stamp) = Self::gen_time(domain.time);
 
-            let res = conn.execute(
-                "INSERT INTO img_meta(id, time, timestamp, sign) values (?1, ?2, ?3, ?4)",
-                params![Self::gen_uuid(), time, stamp, domain.sign],
-            );
-
-            if let Err(_e) = res {
-                tracing::error!("Push img meta records failed!");
-                failed_records.push(domain);
-            }
+            if domain.id.is_none() {
+                conn.execute(
+                    "INSERT INTO img_meta(id, time, timestamp, sign) values (?1, ?2, ?3, ?4)",
+                    params![Self::gen_uuid(), time, stamp, domain.sign],
+                )
+            } else {
+                conn.execute(
+                    "INSERT INTO img_meta(id, time, timestamp, sign) values (?1, ?2, ?3, ?4)",
+                    params![domain.id.unwrap(), time, stamp, domain.sign],
+                )
+            }?;
         }
-
-        if failed_records.len() > 0 {
-            Err(failed_records)
-        } else {
-            Ok(())
-        }
+        Ok(())
     }
 
-    pub fn query_all(&self, offset: usize, limit: usize) -> Result<Vec<ImgMetaDO>, anyhow::Error> {
-        let conn = self.get_conn();
-
+    pub fn query_all(
+        offset: usize,
+        limit: usize,
+        conn: &Connection,
+    ) -> Result<Vec<ImgMetaDO>, anyhow::Error> {
         let mut stmt = conn.prepare("select id, timestamp, sign from img_meta limit ?, ?")?;
         let mut recs = stmt.query([offset, limit])?;
 
@@ -74,7 +87,7 @@ impl ImgMetaDao {
         let sign = row.get(2).unwrap_or("".to_string());
 
         ImgMetaDO {
-            id,
+            id: Some(id),
             time: naive_datetime,
             sign,
         }
@@ -105,15 +118,75 @@ impl ImgMetaDao {
 
 #[cfg(test)]
 mod tests {
-    use super::*;
-    use rusqlite::params;
+    use crate::util::init_console_logger;
 
-    fn set_up() {}
+    use super::*;
+    use rand::{thread_rng, Rng};
+
+    fn set_up() {
+        tracing::info!("Setting up");
+        init_console_logger();
+
+        tracing::info!("Creating table...");
+    }
+
+    fn create_table(conn: &Connection) {
+        ImgMetaDao::create_table(&conn).unwrap();
+    }
+
+    fn gen_conn() -> Connection {
+        let conn = Connection::open_in_memory().unwrap();
+        conn
+    }
+
+    fn tear_down() {
+        tracing::info!("Tearing down");
+    }
+
+    fn gen_test_datas(len: usize) -> Vec<ImgMetaDO> {
+        let mut rng = thread_rng();
+
+        let mut metas = Vec::new();
+        let cur = chrono::Local::now();
+
+        let time = cur.naive_local();
+
+        for i in 1..(len + 1) {
+            let sign = uuid::Uuid::new_v4();
+
+            let sub_millis = rng.gen_range(0..10000i64);
+            let time = time
+                .checked_sub_signed(chrono::Duration::milliseconds(sub_millis))
+                .expect("Gen test naive datetime");
+
+            let meta = ImgMetaDO::with_id(i.to_string(), time, sign.to_string());
+            metas.push(meta);
+        }
+        metas
+    }
 
     #[test]
     fn test_basic_db() -> DbResult<()> {
-        let conn = Connection::open_in_memory()?;
+        set_up();
 
+        let conn = Connection::open_in_memory()?;
+        create_table(&conn);
+
+        let meta_datas = gen_test_datas(100);
+
+        assert!(ImgMetaDao::batch_write(meta_datas, &conn).is_ok());
+
+        let mut id = 1;
+
+        for loop_num in 0..10 {
+            let img_metas = ImgMetaDao::query_all(loop_num * 10, 10, &conn).unwrap();
+            for img in img_metas {
+                assert_eq!(id.to_string(), img.id.unwrap());
+
+                id += 1;
+            }
+        }
+        tear_down();
         Ok(())
     }
 }
--- src/util/mod.rs
+++ src/util/mod.rs
@@ -1,6 +1,20 @@
 use serde::{Deserialize, Serialize};
+use std::sync::Once;
+use tracing::metadata::LevelFilter;
+use tracing_subscriber::{prelude::__tracing_subscriber_SubscriberExt, Layer, Registry};
+
+pub static INIT_LOGGER: Once = Once::new();
 
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
 pub struct GlobalConfig {
     data_dir: String,
+}
+
+pub fn init_console_logger() {
+    INIT_LOGGER.call_once(|| {
+        let stdout_log = tracing_subscriber::fmt::layer().pretty();
+        let subscriber = Registry::default().with(stdout_log.with_filter(LevelFilter::TRACE));
+
+        tracing::subscriber::set_global_default(subscriber).expect("Console log init failed!");
+    });
 }
